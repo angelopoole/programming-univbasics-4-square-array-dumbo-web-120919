27b38f519219f1e3cf638f66391bd32a91dd5ded
Angelo Poole <angelo.poole@outlook.com>
2019-12-05 18:01:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -24,9 +24,10 @@ end
 ###############
 
 def square_array(array)
+  counter = 0
   square = []
   while array[counter] do
-    puts array[counter] ** 2
+    squared.push array[counter] ** 2
     counter +=1
   end
 
